0eac23c47a854acd0ca43d7fa3c017cfaba37383
cjezierski <cjezierski@softia.fr>
2024-04-07 21:33:31 +0200

    test5

--- index.js
+++ index.js
@@ -12,13 +12,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set('view engine', 'ejs');
 app.engine('ejs', require('ejs').__express);
 
-app.get('/alertify.js', function(req, res) {
-    res.sendFile('public/js/alertify.js', {root: __dirname });
-});
-
-app.get('/alertify.css', function(req, res) {
-    res.sendFile('public/style/alertify.css', {root: __dirname });
-});
 
 app.get('/', (req, res) => {
     res.render('pages/index.ejs');
